7b239af46bbc6c6b79de4b1e9f2521ae53b02290
小小怪将军 <104970056+Tarry0520@users.noreply.github.com>
2023-09-10 01:05:51 +0800

    格式化

--- vite-project/src/main.ts
+++ vite-project/src/main.ts
@@ -1,9 +1,9 @@
-import { createApp } from "vue";
-import App from "./App.vue";
+import { createApp } from 'vue'
+import App from './App.vue'
 
-createApp(App).mount("#app");
+createApp(App).mount('#app')
 
 const fun = () => {
-  console.log("这是没有格式化的代码");
-};
-fun();
+  console.log('这是没有格式化的代码')
+}
+fun()
