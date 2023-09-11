17492d51c0f7eb2cc2a4e006246ef99a47708a29
小小怪将军 <104970056+Tarry0520@users.noreply.github.com>
2023-09-11 23:09:36 +0800

    login页面

--- vite-project/src/main.ts
+++ vite-project/src/main.ts
@@ -13,7 +13,10 @@ import 'virtual:svg-icons-register'
 import globalComponent from '@/components'
 //引入路由
 import router from './router'
+//引入仓库
+import pinia from './store'
 //获取应用实例对象
+
 const app = createApp(App)
 app.use(ElementPlus, {
   locale: zhCn, //elementplus国际化
@@ -21,6 +24,8 @@ app.use(ElementPlus, {
 
 //安装自定义插件
 app.use(globalComponent)
+//安装pinia
+app.use(pinia)
 //引入模板路由
 app.use(router)
 //将应用挂载到挂载点上
--- vite-project/src/views/login/index.vue
+++ vite-project/src/views/login/index.vue
@@ -1,7 +1,109 @@
 <template>
-  <div>
-    <h1>我是一级路由登录</h1>
+  <div class="login_container">
+    <el-row>
+      <el-col :span="12" :xs="0"></el-col>
+      <el-col :span="12" :xs="24">
+        <el-form class="login_form">
+          <h1>Hello</h1>
+          <h2>欢迎来到硅谷甄选</h2>
+          <el-form-item>
+            <el-input
+              :prefix-icon="User"
+              v-model="loginForm.username"
+            ></el-input>
+          </el-form-item>
+          <el-form-item>
+            <el-input
+              type="password"
+              :prefix-icon="Lock"
+              v-model="loginForm.password"
+              show-password
+            ></el-input>
+          </el-form-item>
+          <el-form-item>
+            <el-button
+              type="primary"
+              size="default"
+              class="login_btn"
+              @click="login"
+              :loading="loading"
+            >
+              登录
+            </el-button>
+          </el-form-item>
+        </el-form>
+      </el-col>
+    </el-row>
   </div>
 </template>
-<script setup lang="ts"></script>
-<style scoped lang="scss"></style>
+<script setup lang="ts">
+import { User, Lock } from '@element-plus/icons-vue'
+import { reactive, ref } from 'vue'
+import { useRouter } from 'vue-router'
+import { ElNotification } from 'element-plus'
+//引入用户相关的小仓库
+import useUserStore from '@/store/modules/user'
+const useStore = useUserStore()
+//获取路由器
+let $router = useRouter()
+//定义变量控制按钮加载效果
+let loading = ref(false)
+//收集账号和密码的数据
+let loginForm = reactive({ username: 'admin', password: '111111' })
+//登录按钮回调
+const login = async () => {
+  loading.value = true
+  //点击登录按钮之后干什么?
+  //通知仓库发登录请求
+  //请求成功->跳转到首页数据的地方
+  //请求失败->弹出登录失败的信息
+  try {
+    //保证登录成功
+    await useStore.userLogin(loginForm)
+    //编程式导航跳转到首页
+    $router.push('/')
+    //登录成功提示信息
+    ElNotification({
+      type: 'success',
+      message: '登录成功',
+    })
+    loading.value = false
+  } catch (error) {
+    //登录失败加载效果消息
+    loading.value = false
+    //登录失败提示信息
+    ElNotification({
+      type: 'error',
+      message: (error as Error).message,
+    })
+  }
+}
+</script>
+<style scoped lang="scss">
+.login_container {
+  width: 100%;
+  height: 100vh;
+  background: url('@/assets/images/background.jpg') no-repeat;
+  background-size: cover;
+  .login_form {
+    position: relative;
+    width: 80%;
+    top: 30vh;
+    background: url('@/assets/images/login_form.png');
+    background-size: cover;
+    padding: 40px;
+    h1 {
+      color: white;
+      font-size: 40px;
+    }
+    h2 {
+      font-size: 20px;
+      color: white;
+      margin: 20px 0;
+    }
+    .login_btn {
+      width: 100%;
+    }
+  }
+}
+</style>
